dc81acf863a07d00c5e597c7fed06df729a58313
Alexander-Krokhmal <zeddicus.ak@gmail.com>
2023-01-05 21:07:29 +0200

    Add fix

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -1,8 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://639bfed042e3ad692724172b.mockapi.io/';
-
 export const fetchContacts = createAsyncThunk(
     'contacts/fetchAll',
     async (_, thunkAPI) => {
